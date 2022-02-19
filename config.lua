ce6a4c25f58e68f808fb8acf1bd92782dae1e3c3
Aiden <43355457+AidenTheTechBoy@users.noreply.github.com>
2022-02-19 13:39:38 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 -- Authentication Secret: This value can be found in the settings page of the staff panel.
-Config.secret = "xxxxxxxxxxxxxxxxxxxxxxx"
+Config.secret = "XXXXXXXXXXXXXXXXXXXXXXXXX"
 
 -- System Failure Override: If this is set to true, then players will still be able to join if StaffWatch is offline.
 -- *We recommend leaving this enabled in case our systems go down!*
@@ -9,6 +9,3 @@ Config.systemFailureOverride = true
 
 -- Message Color: This is the color of the message that appears in the chat window in RGB format.
 Config.messageColor = {74, 54, 255}
-
--- Ban Appeal Link: This link will be displayed in the ban message, the most common link is a discord link.
-Config.appeal = "appeals.example.com"
